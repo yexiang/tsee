457e78979595236816c31f201b2d4ec37fbf0d91
Your Name <you@example.com>
2018-11-21 17:21:54 +0800

    init

--- src/Tsee.php
+++ src/Tsee.php
@@ -4,7 +4,6 @@ namespace yexiang\tsee;
 class Tsee {
 
   public function __construct() {
-
   }
 
   public function ex($cs) {
@@ -14,6 +13,41 @@ class Tsee {
         $text = str_replace($u->url, '<a href="'.$u->expanded_url.'" target="_blank">'.$u->display_url.'</a>', $text);
       }
     }
-    return $text;
+    if(isset($cs->extended_entities->media)){
+      $temp_media_url = '';
+      $temp_media ='';
+      foreach ($cs->extended_entities->media as $m) {
+        $temp_media_url=$m->url;
+        $temp_media .= '<a href="'.$m->media_url_https.'" target="_blank">'.$m->display_url.'</a> ';
+        if($m->sizes->large->w >= 2046 || $m->sizes->large->h >= 2046){
+          $text .= '<a href="'.$m->media_url_https.':orig" target="_blank"><img src="'.$m->media_url_https.'" class="twimg" /></a>';
+        }
+        elseif($m->sizes->large->w >= 1198 || $m->sizes->large->h >= 1198){
+          $text .= '<a href="'.$m->media_url_https.':large" target="_blank"><img src="'.$m->media_url_https.'" class="twimg" /></a>';
+        }
+        else{
+          $text .= '<img src="'.$m->media_url_https.'" class="twimg" />';
+        }
+        if(isset($m->video_info->variants)){
+          $bitrate=[];
+          foreach($m->video_info->variants as $v){
+            if(strpos($v->url, '.mp4')>0){
+              array_push($bitrate, $v->bitrate);
+            }
+          }
+          sort($bitrate);
+          foreach($m->video_info->variants as $v){
+            if(strpos($v->url, '.mp4')>0){
+              if($v->bitrate==$bitrate[count($bitrate)-1]){
+                $text .= '<video controls="controls" preload="none" src="'. $v->url.'"></video>';
+                $text .= $v->url."\n";
+              }
+            }
+          }
+        }
+      }
+      $text = str_replace($temp_media_url, $temp_media, $text);
+    }
+    return trim($text);
   }
 }

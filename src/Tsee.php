e1fd617a5df348e44e0c21c3c4cbb14f920dc522
Your Name <you@example.com>
2018-11-21 15:46:07 +0800

    init

---
+++ src/Tsee.php
@@ -0,0 +1,19 @@
+<?php
+namespace john\tsee;
+
+class Tsee {
+  public static $OK = 0;
+  public function __construct() {
+
+  }
+  public function ex($cs) {
+    $text='';
+    if(isset($cs->entities->urls)){
+      foreach ($cs->entities->urls as $u) {
+        $text = str_replace($u->url, '<a href="'.$u->expanded_url.'" target="_blank">'.$u->display_url.'</a>', $text);
+      }
+    }
+  
+  }
+    
+}
